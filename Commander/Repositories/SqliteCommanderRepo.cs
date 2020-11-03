53a399dd855f81508ed386641205949988b95093
arbarvar <a_barvar@hotmail.com>
2020-11-03 17:06:03 +0430

    create Sqlite repository

--- Commander/Repositories/SqliteCommanderRepo.cs
+++ Commander/Repositories/SqliteCommanderRepo.cs
@@ -1,10 +1,14 @@
+using System.Collections.Generic;
+using Commander.Data;
 using Commander.Interfaces;
+using Commander.Models;
 
 namespace Commander.Repositories
 {
     
     public class SqliteCommanderRepo : ICommanderRepo
     {
+        private readonly CommanderContext _context;
 
         public SqliteCommanderRepo(CommanderContext context)
         {
@@ -14,12 +18,12 @@ namespace Commander.Repositories
 
         public IEnumerable<Command> GetAllComandes()
         {
-            
+            throw new System.NotImplementedException();
         }
 
         public Command GetCommandByID(int id)
         {
-            
+            throw new System.NotImplementedException();
         }
     }
 }
